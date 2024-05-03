aaa0ece5a369c4b62d7747808802db78758ec010
hungg523 <hoanghung52304@gmail.com>
2024-05-03 14:56:57 +0700

    Add Repositories

---
+++ AppleEShop/Repositories/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using AppleEShop.Data;
+using AppleEShop.Models;
+
+namespace AppleEShop.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppleEShopContext _context;
+
+        public CategoryRepository(AppleEShopContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            var categories = _context.Category.ToList();
+
+            foreach (var category in categories)
+            {
+                category.ProductCount = _context.Product.Count(p => p.CategoryId == category.Id);
+            }
+
+            return categories;
+        }
+    }
+}
