926f7195ae99d7b2eb2bf56c87b6f9d0a21fea48
hungg523 <hoanghung52304@gmail.com>
2024-05-13 22:03:12 +0700

    add feature search and filter products

--- AppleEShop/Controllers/ProductsController.cs
+++ AppleEShop/Controllers/ProductsController.cs
@@ -44,6 +44,24 @@ namespace AppleEShop.Controllers
              );
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Search(string keywords, int productPage = 1)
+        {
+            return View
+            ("Index",
+                new ProductListViewModel
+                {
+                    Product = _context.Product.Where(p => p.Name.Contains(keywords)).Skip((productPage - 1) * PageSize).Take(PageSize),
+                    PagingInfo = new PagingInfo
+                    {
+                        ItemsPerPage = PageSize,
+                        CurrentPage = productPage,
+                        TotalItems = _context.Product.Count()
+                    }
+                }
+             );
+        }
+
         public async Task<IActionResult> Index1()
         {
             IEnumerable<Product> products = _context.Product.Include(p => p.Category); // Lấy danh sách sản phẩm
@@ -75,6 +93,48 @@ namespace AppleEShop.Controllers
             }
             
             return View(product);
+        }
+
+        public async Task<IActionResult> Filter(string category, decimal? priceMin, decimal? priceMax, int productPage = 1)
+        {
+            var stringCate = Request.Query["category"].ToString();
+            ViewBag.Cate = stringCate;
+            IQueryable<Product> query = _context.Product.Include(p => p.Category);
+            decimal minPrice = 1;
+            decimal maxPrice = 9999999;
+            if (query.Count() > 0)
+            {
+                maxPrice = query.Max(x => x.Price);
+                minPrice = query.Min(x => x.Price);
+            }
+            ViewBag.MaxPrice = maxPrice;
+            ViewBag.MinPrice = minPrice;
+            //ViewBag.MaxPriceFilter = (priceMax == null ? maxPrice : priceMax);
+            //ViewBag.MinPriceFilter = (priceMin == null ? minPrice : priceMin);
+            var model = new ProductListViewModel();
+            if (!string.IsNullOrEmpty(stringCate))
+            {
+                query = query.Where(p => stringCate.Contains(p.Category.Name));
+            }
+            if (priceMin.HasValue)
+            {
+                query = query.Where(p => p.Price >= priceMin.Value);
+            }
+
+            if (priceMax.HasValue)
+            {
+                query = query.Where(p => p.Price <= priceMax.Value);
+            }
+            query = query.Skip((productPage - 1) * PageSize).Take(PageSize);
+            var PagingInfo = new PagingInfo
+            {
+                ItemsPerPage = PageSize,
+                CurrentPage = productPage,
+                TotalItems = _context.Product.Count()
+            };
+            model.Product = query;
+            model.PagingInfo = PagingInfo;
+            return View(model);
         }
     }
 }
