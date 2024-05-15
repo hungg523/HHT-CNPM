22c5176349be2e47d29c2da1839d821eee471e13
Tới Văn <tovantoi2003@gmail.com>
2024-05-15 21:52:09 +0700

    add đánh giá sản phẩm và ProductsController

--- AppleEShop/Controllers/ProductsController.cs
+++ AppleEShop/Controllers/ProductsController.cs
@@ -1,8 +1,15 @@
-﻿using AppleEShop.Models.ViewModels;
-using AppleEShop.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AppleEShop.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using AppleEShop.Data;
+using AppleEShop.Models;
+using Microsoft.AspNetCore.Authorization;
+using AppleEShop.Models.ViewModels;
+
 
 namespace AppleEShop.Controllers
 {
@@ -43,7 +50,6 @@ namespace AppleEShop.Controllers
                 }
              );
         }
-
         [HttpPost]
         public async Task<IActionResult> Search(string keywords, int productPage = 1)
         {
@@ -91,7 +97,35 @@ namespace AppleEShop.Controllers
             {
                 return NotFound();
             }
-            
+            var lstComment = await _context.Comment.Join(_context.User,
+                                        c => c.UserId,
+                                        u => u.Id,
+                                        (c, u) => new Comment
+                                        {
+                                            Id = c.Id,
+                                            Rating = c.Rating,
+                                            Content = c.Content,
+                                            DateCreated = c.DateCreated,
+                                            UserId = c.UserId,
+                                            ProductId = c.ProductId,
+                                            UserName = u.Username
+                                        }).Where(x => x.ProductId == id).OrderByDescending(x => x.Id).AsNoTracking().ToListAsync();
+            var modelRating = TotalRating(lstComment);
+            ViewBag.ModelRating = modelRating;
+            double totalRating = 0;
+            if (lstComment != null && lstComment.Count > 0)
+            {
+                totalRating = lstComment.Select(x => (double?)x.Rating).Average() ?? 0;
+            }
+            ViewBag.total = modelRating.TotalRating;
+            int pageSize = 10;
+            ViewBag.TotalPages = (int)Math.Ceiling((double)lstComment.Count / pageSize);
+            int pageNumber = page ?? 1;
+            int startIndex = (pageNumber - 1) * pageSize;
+            ViewBag.TotalRating = Math.Round(totalRating, 1);
+            lstComment = lstComment.Skip(startIndex).Take(pageSize).ToList();
+            ViewBag.ListComment = lstComment;
+            ViewBag.CurrentPage = pageNumber;
             return View(product);
         }
 
@@ -135,6 +169,168 @@ namespace AppleEShop.Controllers
             model.Product = query;
             model.PagingInfo = PagingInfo;
             return View(model);
+        }
+
+        // GET: Products/Create
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Detail,Price,Image,Color,Quantity,IsNew,IsBestSeller,CategoryId,DiscountId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Detail,Price,Image,Color,Quantity,IsNew,IsBestSeller,CategoryId,DiscountId")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Product == null)
+            {
+                return Problem("Entity set 'AppleEShopContext.Product'  is null.");
+            }
+            var product = await _context.Product.FindAsync(id);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return (_context.Product?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+        [HttpPost]
+        public ActionResult RatingComment(Comment comment)
+        {
+            if (comment.UserId == 0 || comment.ProductId == 0)
+            {
+                return NotFound(); ;
+            }
+            comment.DateCreated = DateTime.Now;
+            _context.Comment.Add(comment);
+            _context.SaveChanges();
+            return Redirect("/Products/Details/" + comment.ProductId);
+        }
+        public CountRatingModel TotalRating(List<Comment> cmt)
+        {
+            var countRating = new CountRatingModel();
+            if (cmt != null && cmt.Count > 0)
+            {
+                countRating.TotalRating = cmt.Count();
+                countRating.TotalOne = cmt.Where(x => x.Rating == 1).Count();
+                countRating.TotalTwo = cmt.Where(x => x.Rating == 2).Count();
+                countRating.TotalThree = cmt.Where(x => x.Rating == 3).Count();
+                countRating.TotalFour = cmt.Where(x => x.Rating == 4).Count();
+                countRating.TotalFive = cmt.Where(x => x.Rating == 5).Count();
+                double pecentOne = ((double)countRating.TotalOne / (double)countRating.TotalRating) * 100;
+                double pecentTwo = ((double)countRating.TotalTwo / (double)countRating.TotalRating) * 100;
+                double pecentThree = ((double)countRating.TotalThree / (double)countRating.TotalRating) * 100;
+                double pecentFour = ((double)countRating.TotalFour / (double)countRating.TotalRating) * 100;
+                double pecentFive = ((double)countRating.TotalFive / (double)countRating.TotalRating) * 100;
+                countRating.PercentOne = Math.Round(pecentOne, 0);
+                countRating.PercentTwo = Math.Round(pecentTwo, 0);
+                countRating.PercentThree = Math.Round(pecentThree, 0);
+                countRating.PercentFour = Math.Round(pecentFour, 0);
+                countRating.PercentFive = Math.Round(pecentFive, 0);
+            }
+            return countRating;
         }
     }
 }
